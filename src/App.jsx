71858e0f993827ef2aae9bf7bffb9bf93b0812ea
std60010912540@acc.msu.ac.th <nathakritbc602@gmail.com>
2023-09-21 16:33:40 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Table } from "antd";
+const ApiUrl = `https://jsonplaceholder.typicode.com`;
+
+export default function App() {
+  const [posts, setPosts] = useState([]);
+
+  const fetchPostAll = async () => {
+    try {
+      const { data, status } = await axios.get(`${ApiUrl}/posts`);
+      if (status === 200) {
+        // console.log("data", data);
+        setPosts(data);
+      }
+    } catch (error) {
+      console.error("fetching error!...", error);
+    }
+  };
+
+  const columns = [
+    {
+      title: "Id",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "UserId",
+      dataIndex: "userId",
+      key: "userId",
+    },
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+    },
+  ];
+
+  useEffect(() => {
+    // return () => {
+    fetchPostAll();
+    // };
+  }, []);
+
+  return (
+    <>
+      <h1>Posts Data</h1>
+      <Table dataSource={posts} columns={columns} />;
+    </>
+  );
+}
